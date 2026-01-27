08a1f10bc564fe03e73450815c3bcb30baadf936
Sumira Naroola <snnaro26@colby.edu>
2026-01-27 17:42:38 +0000

    adding assignment one and checking that it is committed properly. brickman sst at buoy m01

---
+++ assignments/assignment_script_1.R
@@ -0,0 +1,30 @@
+# assignment_script_1.R
+source("setup.R")
+
+# bouy m01
+m01 <- gom_buoys() |> dplyr::filter(id == "M01")
+
+# brickman data
+x <- brickman_database() |>
+  dplyr::filter(scenario == "RCP45",
+                year == 2055,
+                interval == "mon") |>
+  read_brickman()
+
+# extract wide form vals
+sst <- extract_brickman(x, m01, form = "wide")
+
+# order months
+sst$month <- factor(
+  sst$month,
+  levels = c("Jan","Feb","Mar","Apr","May","Jun","Jul","Aug","Sep","Oct","Nov","Dec")
+)
+
+# plot
+ggplot2::ggplot(sst, ggplot2::aes(x = month, y = SST)) +
+  ggplot2::geom_point() +
+  ggplot2::labs(
+    title = "RCP4.5 2055 SST at buoy M01",
+    x = "Month",
+    y = "SST (C)"
+  )
